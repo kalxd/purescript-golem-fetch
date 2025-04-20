21a7631aa4a3b84962d7c89dacf66158bfd05d60
荀洪道 <kalxd@126.com>
2025-04-20 15:09:43 +0800

    再添加几个函数。

--- src/Network/Http/Internal/Url.js
+++ src/Network/Http/Internal/Url.js
@@ -19,6 +19,14 @@ export const _search_delete = key => search => {
 	return clone;
 };
 
+export const _search_entries = tuple => search => {
+	let xs = [];
+	for (const [a, b] of search.entries()) {
+		xs.push(tuple(a)(b));
+	}
+	return xs;
+};
+
 export const _search_get = just => nothing => key => search => {
 	const v = search.get(key);
 	if (v === null) {
@@ -28,6 +36,28 @@ export const _search_get = just => nothing => key => search => {
 		return just(v);
 	}
 };
+
+export const _search_getAll = key => search => search.getAll(key);
+
+export const _search_has = key => search => search.has(key);
+
+export const _search_keys = search => {
+	let xs = [];
+	for (const k of search.keys()) {
+		xs.push(k);
+	}
+	return xs;
+};
+
+export const _search_set = key => value => search => {
+	let clone = _search_make(search);
+	clone.set(key, value);
+	return clone;
+};
+
+export const _search_size = search => search.size;
+
+export const _search_values = search => search.values();
 /** end URLSearchParams */
 
 export const _url = input => new url.URL(input);
--- src/Network/Http/Internal/Url.purs
+++ src/Network/Http/Internal/Url.purs
@@ -3,6 +3,7 @@ module Network.Http.Internal.Url where
 import Prelude
 
 import Data.Maybe (Maybe)
+import Data.Tuple (Tuple)
 
 foreign import data URLSearchParams :: Type
 
@@ -19,7 +20,21 @@ foreign import _search_append :: String -> String -> URLSearchParams -> URLSearc
 
 foreign import _search_delete :: String -> URLSearchParams -> URLSearchParams
 
+foreign import _search_entries :: (String -> String -> Tuple String String) -> URLSearchParams -> Array (Tuple String String)
+
 foreign import _search_get :: (String -> Maybe String) -> Maybe String -> String -> URLSearchParams -> Maybe String
+
+foreign import _search_getAll :: String -> URLSearchParams -> Array String
+
+foreign import _search_has :: String -> URLSearchParams -> Boolean
+
+foreign import _search_keys :: URLSearchParams -> Array String
+
+foreign import _search_set :: String -> String -> URLSearchParams -> URLSearchParams
+
+foreign import _search_size :: URLSearchParams -> Int
+
+foreign import _search_values :: URLSearchParams -> Array String
 
 foreign import data URL :: Type
 
