2a9cbf9fa54bc08b158589b8aafefb434113e224
荀洪道 <kalxd@126.com>
2025-04-21 18:27:48 +0800

    Headers模块。

---
+++ src/Node/Fetch/Headers.purs
@@ -0,0 +1,38 @@
+module Node.Fetch.Headers ( module Node.Fetch.Headers
+                          , module Output
+                          ) where
+
+import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple(..))
+import Node.Fetch.Unsafe.Fetch (Headers) as Output
+import Node.Fetch.Unsafe.Fetch as F
+
+empty :: F.Headers
+empty = F._headers_empty
+
+fromRecord :: forall r. {|r} -> F.Headers
+fromRecord = F._headers_make
+
+insert :: String -> String -> F.Headers -> F.Headers
+insert = F._headers_set
+
+append :: String -> String -> F.Headers -> F.Headers
+append = F._headers_append
+
+delete :: String -> F.Headers -> F.Headers
+delete = F._headers_delete
+
+member :: String -> F.Headers -> Boolean
+member = F._headers_has
+
+lookup :: String -> F.Headers -> Maybe String
+lookup = F._headers_get Just Nothing
+
+toArray :: F.Headers -> Array (Tuple String String)
+toArray = F._headers_entries Tuple
+
+keys :: F.Headers -> Array String
+keys = F._headers_keys
+
+values :: F.Headers -> Array String
+values = F._headers_values
