f9d334389970465c7255355e3f7a153efbb8a634
荀洪道 <kalxd@126.com>
2025-04-21 10:42:07 +0800

    精准导出。

--- src/Network/AHttp/URL.purs
+++ src/Network/AHttp/URL.purs
@@ -1,1 +1,9 @@
-module Network.AHttp.URL where
+module Network.AHttp.URL ( module Network.AHttp.URL
+                         , module Output
+                         ) where
+
+import Network.AHttp.Internal.URL as URL
+import Network.AHttp.Internal.URL (URL) as Output
+
+fromString :: String -> URL.URL
+fromString = URL._url_make
