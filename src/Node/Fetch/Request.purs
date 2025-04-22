7a64250494f1a030cd643056a4ef5f4c1225fefa
荀洪道 <kalxd@126.com>
2025-04-22 15:50:59 +0800

    添加转化函数。

--- src/Node/Fetch/Request.purs
+++ src/Node/Fetch/Request.purs
@@ -13,6 +13,7 @@ import Prelude
 
 import Data.Maybe (Maybe(..))
 import Node.Fetch.Headers as H
+import Node.Fetch.Unsafe.Fetch as F
 
 data RequestCache = RequestCacheDefault
                   | RequestCacheNoStore
@@ -22,6 +23,22 @@ data RequestCache = RequestCacheDefault
                   | RequestCacheOnlyIfCached
 
 derive instance Eq RequestCache
+
+instance Show RequestCache where
+  show RequestCacheDefault = "RequestCacheDefault"
+  show RequestCacheNoStore = "RequestCacheNoStore"
+  show RequestCacheReload = "RequestCacheReload"
+  show RequestCacheNoCache = "RequestCacheNoCache"
+  show RequestCacheForceCache = "RequestCacheForceCache"
+  show RequestCacheOnlyIfCached = "RequestCacheOnlyIfCached"
+
+cacheToString :: RequestCache -> String
+cacheToString RequestCacheDefault = "default"
+cacheToString RequestCacheNoStore = "no-store"
+cacheToString RequestCacheReload = "reload"
+cacheToString RequestCacheNoCache = "no-cache"
+cacheToString RequestCacheForceCache = "force-cache"
+cacheToString RequestCacheOnlyIfCached = "only-if-cached"
 
 data RequestCredentials = RequestCredentialsOmit
                         | RequestCredentialsSameOrigin
@@ -84,4 +101,10 @@ defaultRequest = Request { body: Nothing
                          , priority: Nothing
                          , redirect: Nothing
                          , referrer: Nothing
+                         }
+
+requestToInit :: Request -> F.Request
+requestToInit (Request req) = F.unwrapRequestToInit req'
+  where req' = F.Request { body: req.body
+                         , cache: map cacheToString req.cache
                          }
