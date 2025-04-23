5df4e1fd4ea58b54fec54dde8c0cfbab878e4701
荀洪道 <kalxd@126.com>
2025-04-23 14:13:03 +0800

    Response。

---
+++ src/Node/Fetch/Response.purs
@@ -0,0 +1,66 @@
+module Node.Fetch.Response ( module Output
+                           , ResponseType(..)
+                           , bodyUsed
+                           , headers
+                           , ok
+                           , redirected
+                           , status
+                           , responseType
+                           , json
+                           , text
+                           ) where
+
+import Prelude
+
+import Control.Promise (toAffE)
+import Data.Argonaut (class DecodeJson, JsonDecodeError, decodeJson)
+import Data.Either (Either)
+import Effect.Aff (Aff)
+import Node.Fetch.Unsafe.Fetch (Response) as Output
+import Node.Fetch.Unsafe.Fetch as F
+
+data ResponseType = ResponseTypeBasic
+                  | ResponseTypeCors
+                  | ResponseTypeError
+                  | ResponseTypeOpaque
+                  | ResponseTypeOpaqueRedirect
+
+instance Show ResponseType where
+  show ResponseTypeBasic = "ResponseTypeBasic"
+  show ResponseTypeCors = "ResponseTypeCors"
+  show ResponseTypeError = "ResponseTypeError"
+  show ResponseTypeOpaque = "ResponseTypeOpaque"
+  show ResponseTypeOpaqueRedirect = "ResponseTypeOpaqueRedirect"
+
+derive instance Eq ResponseType
+
+typeFromString :: String -> ResponseType
+typeFromString "cors" = ResponseTypeCors
+typeFromString "error" = ResponseTypeError
+typeFromString "opaque" = ResponseTypeOpaque
+typeFromString "opaqueredirect" = ResponseTypeOpaqueRedirect
+typeFromString _ = ResponseTypeBasic
+
+bodyUsed :: F.Response -> Boolean
+bodyUsed = F._response_bodyUsed
+
+headers :: F.Response -> F.Headers
+headers = F._response_headers
+
+ok :: F.Response -> Boolean
+ok = F._response_ok
+
+redirected :: F.Response -> Boolean
+redirected = F._response_redirected
+
+status :: F.Response -> Int
+status = F._response_status
+
+responseType :: F.Response -> ResponseType
+responseType = typeFromString <<< F._response_type
+
+json :: forall json. DecodeJson json => F.Response -> Aff (Either JsonDecodeError json)
+json rsp = decodeJson <$> (toAffE $ F._response_json rsp)
+
+text :: F.Response -> Aff String
+text = toAffE <<< F._response_text
